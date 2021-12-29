5124d051f87badd3646b9c8a649e9eaaa2f25626
Alde Rojas <hello@alde.io>
2021-12-29 00:02:40 -0600

    Comment out path, it causes problem when typing paths under /dev

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -28,7 +28,7 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
-    { name = 'path' },
+    -- { name = 'path' },
   }, {
     { name = 'buffer' },
   }),
